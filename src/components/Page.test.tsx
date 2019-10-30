be70e145f8f198f45616932d3989fbd7cf032f43
F <f@dxw.com>
2019-10-30 14:55:06 +0000

    Set Page up to accept components as props
    We want to make be able to specify components to be constructed at
    runtime, so that we can by dynamic in their properties.
    
    This commit adds `prop-types` as a dependency, so the libraries we build
    will advise on prop types even if the consumer isn't using TypeScript.

--- src/components/Page.test.tsx
+++ src/components/Page.test.tsx
@@ -1,10 +1,69 @@
-import React from "react";
+import PropTypes, { ValidationMap } from "prop-types";
+import React, { FunctionComponent, ImgHTMLAttributes } from "react";
 import renderer from "react-test-renderer";
 
-import Page from "./Page";
+import Page, { PageComponent, PageComponents } from "./Page";
 
-it("renders correctly", () => {
-  const component = renderer.create(<Page />);
+interface TestProps {
+  content: string;
+}
+
+class TestClassComponent extends React.Component<TestProps> {
+  static propTypes: ValidationMap<TestProps> = {
+    content: PropTypes.string.isRequired
+  };
+
+  render(): JSX.Element {
+    const { content } = this.props;
+
+    return <div>{content}</div>;
+  }
+}
+
+const TestFunctionComponent: FunctionComponent<TestProps> = ({ content }) => (
+  <div>{content}</div>
+);
+
+TestFunctionComponent.propTypes = {
+  content: PropTypes.string.isRequired
+};
+
+it("renders correctly with all props", () => {
+  const component = renderer.create(
+    <Page
+      components={
+        [
+          {
+            id: "test-div",
+            Component: "div"
+          } as PageComponent,
+          {
+            id: "test-img",
+            Component: "img",
+            props: {
+              src: "test.png"
+            }
+          } as PageComponent<ImgHTMLAttributes<HTMLImageElement>>,
+          {
+            id: "test-class",
+            Component: TestClassComponent,
+            props: {
+              content: "test class content"
+            }
+          } as PageComponent<TestProps>,
+          {
+            id: "test-function",
+            Component: TestFunctionComponent,
+            props: {
+              content: "test function content"
+            }
+          } as PageComponent<TestProps>
+        ] as PageComponents<
+          {} | ImgHTMLAttributes<HTMLImageElement> | TestProps
+        >
+      }
+    />
+  );
 
   expect(component).toMatchSnapshot();
 });
