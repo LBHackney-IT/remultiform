c136eb9683420d4b221003d33b21080e9c18b460
F <f@dxw.com>
2019-11-09 18:00:17 +0000

    Update matchers to ignore all __*__ directories
    Any __*__ directory isn't intended to be part of the library itself,
    rather tests, fixtures, mocks, test helpers, etc.

--- examples/next-kittens/jest.config.js
+++ examples/next-kittens/jest.config.js
@@ -4,6 +4,7 @@ const babelConfig = require("./babel.config");
 module.exports = {
   preset: "ts-jest/presets/js-with-ts",
   restoreMocks: true,
+  testMatch: ["<rootDir>/__tests__/**/*.+(spec|test).ts?(x)"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   globals: {
     "ts-jest": {
