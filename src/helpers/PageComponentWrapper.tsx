2b2ed6169f633b673b9a6af62d175f474246f483
F <f@dxw.com>
2019-11-14 15:06:03 +0000

    Document the components, converting them to function components
    Class components generate a lot of internal documentation for their
    methods and properties that an end user doesn't need. I have changed
    them into function components, as we don't have state on them, so class
    components are overkill anyway.

--- src/helpers/PageComponentWrapper.tsx
+++ src/helpers/PageComponentWrapper.tsx
@@ -1,35 +1,85 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+/**
+ * A component along with the associated properties needed to render it as part
+ * of a {@link Step}.
+ *
+ * ```ts
+ * const component: PageComponent<typeof MyInput> = {
+ *   key: "my-input",
+ *   Component: MyInput,
+ *   props: {
+ *     defaultValue: "Enter something?"
+ *   }
+ * }
+ * ```
+ */
 export interface PageComponent<
-  C extends React.ElementType<P>,
-  P = C extends React.ElementType<infer T> ? T : {}
+  ComponentType extends React.ElementType<Props>,
+  Props = ComponentType extends React.ElementType<infer T> ? T : {}
 > {
+  /**
+   * A unique identifier for this component on the page.
+   */
   key: React.Key;
-  Component: C;
-  props: JSX.LibraryManagedAttributes<C, P>;
+
+  /**
+   * The component class, function, or tag name to render.
+   */
+  Component: ComponentType;
+
+  /**
+   * The props to pass to {@link PageComponent.Component}.
+   */
+  props: JSX.LibraryManagedAttributes<ComponentType, Props>;
 }
 
+/**
+ * A wrapper for {@link PageComponent} for a {@link Step}.
+ *
+ * Create these using {@link wrapPageComponent}.
+ */
 export interface PageComponentWrapper {
   key: React.Key;
+
+  /**
+   * The function to render the component.
+   *
+   * @param key - The key to give the generated component. Defaults to
+   * {@link PageComponentWrapper.key}.
+   */
   render(key?: React.Key): JSX.Element;
 }
 
+/**
+ * The proptypes of a {@link PageComponentWrapper}.
+ */
 export const pageComponentWrapperPropType = PropTypes.exact({
   key: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   render: PropTypes.func.isRequired
 }).isRequired;
 
+/**
+ * Wrap a {@link PageComponent} in a {@link PageComponentWrapper} ready to be
+ * included in a {@link Step}.
+ */
+// This method mostly exists so we can have strict type inference on our
+// components. By wrapping the component in a generic function, the type of the
+// component has to be infered in order to call the function, meaning we get
+// strong types with little effort.
 export const wrapPageComponent = <
-  C extends React.ElementType<P>,
-  P = C extends React.ElementType<infer T> ? T : {}
->({
-  key,
-  Component,
-  props
-}: PageComponent<C, P>): PageComponentWrapper => ({
-  key,
-  render(k?: React.Key): JSX.Element {
-    return <Component key={k !== undefined ? k : key} {...props} />;
-  }
-});
+  ComponentType extends React.ElementType<Props>,
+  Props = ComponentType extends React.ElementType<infer T> ? T : {}
+>(
+  component: PageComponent<ComponentType, Props>
+): PageComponentWrapper => {
+  const { key, Component, props } = component;
+
+  return {
+    key,
+    render(k: React.Key = key): JSX.Element {
+      return <Component key={k} {...props} />;
+    }
+  };
+};
