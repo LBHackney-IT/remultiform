b58c8673ecaef0a8faf099e0fa28451a68b03856
F <f@dxw.com>
2019-11-19 13:56:36 +0000

    Strip local absolute paths out of snapshots
    We don't want CI to fail tests because snapshots have absolute paths in
    them, and they are very unlikely to be important to a test.

---
+++ src/__tests__/serializers/pathSerializer.js
@@ -0,0 +1,13 @@
+const { resolve } = require("path");
+
+const rootDirName = resolve(__dirname, "..", "..", "..");
+
+module.exports = {
+  test(value) {
+    return value && typeof value === "string" && value.includes(rootDirName);
+  },
+
+  print(value, serialize) {
+    return serialize(value.replace(new RegExp(rootDirName, "g"), "<rootDir>"));
+  }
+};
